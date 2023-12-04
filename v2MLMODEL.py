b2b53e23dfb0752195563aef67a38b4320513291
tellyvisionr <tellyreames2@gmail.com>
2023-12-03 18:56:32 -0800

    Updated to use ONE csv files

--- v2MLMODEL.py
+++ v2MLMODEL.py
@@ -3,37 +3,31 @@ from sklearn.model_selection import train_test_split, RandomizedSearchCV
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.metrics import accuracy_score, classification_report
 
-# Load and Explore Data
-symptom_data = pd.read_csv("Covid 19 symptoms data UPDATED.csv")
-covid_data = pd.read_csv("Covid Data UPDATED.csv")
+# Load the first 200 rows of the merged data directly
+merged_data = pd.read_csv("merged_data.csv")
 
-# Randomize the data
-symptom_data_randomized = symptom_data.sample(frac=1, random_state=20)
-covid_data_randomized = covid_data.sample(frac=1, random_state=20)
+# Shuffle the rows randomly
+merged_data_randomized = merged_data.sample(frac=1, random_state=20)
 
-# Reduce the size to 100 rows each
-symptom_data_sampled = symptom_data_randomized.head(100)
-covid_data_sampled = covid_data_randomized.head(100)
-
-# Merge datasets using common identifiers
-merged_data = pd.merge(symptom_data_sampled, covid_data_sampled, on=['Age_0-9', 'Age_10-19', 'Age_20-24', 'Age_25-59', 'Age_60+', 'SEX'])
+# Select the first 100 rows
+merged_data_subset = merged_data_randomized.head(100)
 
 # Feature Selection
-X = merged_data[['Fever', 'Tiredness', 'Dry-Cough', 'Difficulty-in-Breathing', 'Sore-Throat',
-                 'None_Sympton', 'Age_0-9', 'Age_10-19', 'Age_20-24', 'Age_25-59', 'Age_60+',
-                 'Severity_None', 'Severity_Mild', 'Severity_Moderate', 'Severity_Severe',
-                 'SEX', 'DIABETES', 'OBESITY', 'ASTHMA', 'TOBACCO']]
-y = merged_data['HAS_COVID']
+X = merged_data_subset[['Fever', 'Tiredness', 'Dry-Cough', 'Difficulty-in-Breathing', 'Sore-Throat',
+                        'None_Symptom', 'Age_0-9', 'Age_10-19', 'Age_20-24', 'Age_25-59', 'Age_60+',
+                        'Severity_None', 'Severity_Mild', 'Severity_Moderate', 'Severity_Severe',
+                        'SEX', 'DIABETES', 'OBESITY', 'ASTHMA', 'TOBACCO']]
+y = merged_data_subset['HAS_COVID']
 
 # Train-Test Split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.7, random_state=42)
 
 # Define an extended search space (can be adjusted based on your specific requirements)
 param_dist = {
     'n_estimators': [50, 100, 150, 200],
     'max_depth': [3, 5, 7, 10],
     'learning_rate': [0.01, 0.05, 0.1, 0.2],
-    'subsample': [0.8, 0.9, 1.0],
+    'subsample': [0.6, 0.7, 0.8, 0.9, 1.0],  # Adjust the subsample values
     'min_samples_split': [2, 5, 10],
     'min_samples_leaf': [1, 2, 4]
 }
@@ -47,13 +41,13 @@ random_search.fit(X_train, y_train)
 
 # Print the best parameters found by RandomizedSearchCV
 print(f"Model: {model.__class__.__name__}")
-print("Best Parameters:", random_search.best_params_)
+print("Best Parameters (RandomizedSearchCV):", random_search.best_params_)
 
-# Build and Train the Model with the best parameters
+# Build and Train the Model with the best parameters from RandomizedSearchCV
 best_model_random = random_search.best_estimator_
 best_model_random.fit(X_train, y_train)
 
-# Evaluate the Model
+# Evaluate the Model on the original test set
 y_pred_random = best_model_random.predict(X_test)
 accuracy_random = accuracy_score(y_test, y_pred_random)
 report_random = classification_report(y_test, y_pred_random)
